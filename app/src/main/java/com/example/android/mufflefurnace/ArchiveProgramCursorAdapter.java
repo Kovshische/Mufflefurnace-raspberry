8ea36e7d6f877ba559bd1c4f6ff6b427f655fc86
Valeriy <Kovshische@gmail.com>
2018-03-19 18:52:28 +0200

    Add ArchiveProgramCursorAdapter

---
+++ app/src/main/java/com/example/android/mufflefurnace/ArchiveProgramCursorAdapter.java
@@ -0,0 +1,48 @@
+package com.example.android.mufflefurnace;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.android.mufflefurnace.Data.ProgramContract;
+
+/**
+ * Created by admin on 3/19/2018.
+ */
+
+public class ArchiveProgramCursorAdapter extends CursorAdapter {
+
+
+    public ArchiveProgramCursorAdapter(Context context,Cursor cursor){
+        super(context, cursor, 0);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.list_program, parent,false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+        TextView textViewName  = (TextView) view.findViewById(R.id.program_name);
+        TextView textViewStarted = (TextView) view.findViewById(R.id.program_created);
+
+        //Extract properties from cursor
+        String name = cursor.getString(cursor.getColumnIndexOrThrow(ProgramContract.ProgramEntry.COLUMN_A_PROGRAM_NAME));
+        //String created = cursor.getInt(cursor.getColumnIndexOrThrow(ProgramContract.ProgramEntry.COLUMN_CREATED_AT));
+        String created  = cursor.getString(cursor.getColumnIndexOrThrow(ProgramContract.ProgramEntry.COLUMN_STARTED_AT));
+
+        final int program_id = cursor.getInt(cursor.getColumnIndexOrThrow(ProgramContract.ProgramEntry._ID));
+
+        textViewName.setText(name);
+        textViewStarted.setText(created);
+        // textViewCreated.setText(created);
+
+
+    }
+}
--- app/src/main/java/com/example/android/mufflefurnace/ArchiveProgramsActivity.java
+++ app/src/main/java/com/example/android/mufflefurnace/ArchiveProgramsActivity.java
@@ -4,9 +4,14 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+
+import com.example.android.mufflefurnace.Data.ProgramContract;
 
 /**
  * Created by admin on 3/19/2018.
@@ -15,6 +20,7 @@ import android.view.View;
 public class ArchiveProgramsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private static String LOG_TAG = AppCompatActivity.class.getSimpleName();
+    private static final int PROGRAM_LOADER = 0;
 
     @Override
     protected void onCreate (Bundle savedInstanceState){
@@ -23,12 +29,35 @@ public class ArchiveProgramsActivity extends AppCompatActivity implements Loader
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setVisibility(View.GONE);
+
+        RelativeLayout emptyView =(RelativeLayout) findViewById(R.id.empty_view);
+        emptyView.setVisibility(View.GONE);
+
+        //find the ListView which will be populated with the pet data
+        ListView programListView = (ListView) findViewById(R.id.list_view_programs);
+
+        getSupportLoaderManager().initLoader(PROGRAM_LOADER, null, this);
     }
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return null;
+        // define the projection that specifies the column from the table we care about.
+        String[] projection = {
+                ProgramContract.ProgramEntry._ID,
+                ProgramContract.ProgramEntry.COLUMN_A_PROGRAM_NAME,
+                ProgramContract.ProgramEntry.COLUMN_STARTED_AT};
+        // This loader will execute the ContentProvider's query method on a background thread
+
+        return new CursorLoader(this, //parent activity content
+                ProgramContract.ProgramEntry.CONTENT_URI_A_PROGRAMS,   //Provider content URI to query
+                projection,             //Columns to include in the resulting Cursor
+                null,                   //No selection clause
+                null,                   //No selection arguments
+                null);                   //Default sort order
+
+
     }
+
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
